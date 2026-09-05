ec18d8ba2e8ef7a6f2c34fc9c44f889b1da18e07
Thallapally Vineeth <164376419+vineethGoudGithub@users.noreply.github.com>
2026-09-05 12:16:58 +0530

    Fix formatting in mvnw.cmd comments

--- mvnw.cmd
+++ mvnw.cmd
@@ -5,7 +5,7 @@
 @REM distributed with this work for additional information
 @REM regarding copyright ownership.  The ASF licenses this file
 @REM to you under the Apache License, Version 2.0 (the
-@REM "License"); you may not use this file except in compliance
+@REM "License"); you may not use this file except in compliance 
 @REM with the License.  You may obtain a copy of the License at
 @REM
 @REM    http://www.apache.org/licenses/LICENSE-2.0
